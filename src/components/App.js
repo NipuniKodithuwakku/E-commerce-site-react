0800971da44e09f8df5d161e0ec486375ce0d868
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-07-31 18:46:50 +0530

    add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,27 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route } from "react-router-dom";
 
-const App = () => {
-  return <div>App component</div>;
-};
+import Cart from "./Cart";
+import Default from "./Default";
+import Details from "./Details";
+import Navbar from "./Navbar";
+// import Product from "./Product";
+import ProductList from "./ProductList";
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.querySelector("#root")
+);
