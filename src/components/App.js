a6f902d4827e05fccfbe2598f2561b3021d64d80
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-08-01 03:35:18 +0530

    modify navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route } from "react-router-dom";
-
 import Cart from "./Cart";
 import Default from "./Default";
 import Details from "./Details";
 import Navbar from "./Navbar";
-// import Product from "./Product";
 import ProductList from "./ProductList";
 
 class App extends React.Component {
@@ -15,7 +13,7 @@ class App extends React.Component {
       <React.Fragment>
         <Navbar />
         <Switch>
-          <Route path="/" component={ProductList} />
+          <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />
           <Route path="/cart" component={Cart} />
           <Route component={Default} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+// import store from "./store.svg";
 
 export default class Navbar extends Component {
   render() {
-    return <div>Navbar component</div>;
+    return (
+      <nav className="navbar navbar-expand-sm navbar-light bg-secondary px-sm-5">
+        {/* <Link to="/">
+          <img src="img/store.png" alt="store" className="navbar-brand" />
+        </Link> */}
+        <Link exact to="/">
+          <span className="navbar-brand mb-0 h1">e-pola</span>
+        </Link>
+
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="./Cart" className="ml-auto">
+          <button className="btn btn-outline-warning my-2 my-sm-0">
+            <i class="fa fa-cart-plus mr-2"></i>my cart
+          </button>
+        </Link>
+      </nav>
+    );
   }
 }
