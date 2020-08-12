79173574491432abeca3d8bbc09a9fd105871eea
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-08-12 20:54:53 +0530

    make detail product dynamic

--- src/components/Details.js
+++ src/components/Details.js
@@ -26,10 +26,42 @@ export default class Details extends Component {
               </div>
               {/*product info*/}
               <div className="row">
-                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                <div className="col-10 mx-auto col-md-6 my-3 ">
                   <img src={img} className="img-fluid" alt="product" />
                 </div>
-                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize"></div>
+                {/*product text*/}
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h2>model: {title}</h2>
+                  <h4 className="text-title  text-muted mt-3 mb-2">
+                    made by:<span className="text-uppercase">{company}</span>
+                  </h4>
+                  <h4 className="text-yellow">
+                    <strong>
+                      price:<span>$</span>
+                      {price}
+                    </strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    some info about product:
+                  </p>
+                  <p className="text-muted lead">{info}</p>
+                  {/*buttons*/}
+                  <div>
+                    <Link to="/">
+                      <ButtonContainer>back to products</ButtonContainer>
+                    </Link>
+
+                    <ButtonContainer
+                      cart
+                      disabled={inCart ? true : false}
+                      onClick={() => {
+                        value.addToCart(id);
+                      }}
+                    >
+                      {inCart ? "inCart" : "add to cart"}
+                    </ButtonContainer>
+                  </div>
+                </div>
               </div>
             </div>
           );
--- src/context.js
+++ src/context.js
@@ -23,8 +23,16 @@ class ProductProvider extends Component {
       };
     });
   };
-  handleDetail = () => {
-    console.log("hello from detail");
+
+  getItem = (id) => {
+    const product = this.state.products.find((item) => item.id === id);
+    return product;
+  };
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
   addToCart = () => {
     console.log("hello from add to cart");
