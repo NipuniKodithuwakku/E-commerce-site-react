8798adf8c1724fb49cb58410717b5c16b20b95e2
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-09-03 08:03:12 +0530

    add removeItem function

--- src/context.js
+++ src/context.js
@@ -76,7 +76,26 @@ class ProductProvider extends Component {
     console.log("this is decrement method");
   };
   removeItem = (id) => {
-    console.log("item removed");
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter((item) => item.id !== id);
+
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          products: [...tempProducts],
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
   clearCart = () => {
     this.setState(
@@ -86,7 +105,7 @@ class ProductProvider extends Component {
         };
       },
       () => {
-        setProducts();
+        this.setProducts();
         this.addTotals();
       }
     );
